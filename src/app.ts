8720dffa3dd04bd4f72575ad51372053f7a0a371
Staś Werno <89650713+staswerno@users.noreply.github.com>
2022-03-30 18:25:13 +0200

    add intersecrion types

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,27 @@
-// Code goes here!
+// intersection types
+// similar to interfaces, bit less code
+
+type Admin = {
+	name: string;
+	privileges: string[];
+};
+
+type Employee = {
+	name: string;
+	startDate: Date;
+};
+
+// combo of above types
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+	name: "Stasi",
+	privileges: ["dungeon-master"],
+	startDate: new Date(),
+};
+
+// can do any types
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
